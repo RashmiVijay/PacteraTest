3d2277cfa7062d6743115e41128361a6e325e4a6
RashmiVijay <thisisrashmi@gmail.com>
2015-05-10 10:08:03 +1000

    Setter and Getter Functions for JSON object values

---
+++ FactsAdapter.java
@@ -0,0 +1,89 @@
+package com.test.pacteraandroidtest;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class FactsAdapter extends ArrayAdapter<Facts> {
+	ArrayList<Facts> factList;
+	LayoutInflater vi;
+	int Resource;
+	ViewHolder holder;
+
+	public FactsAdapter(Context context, int resource, ArrayList<Facts> objects) {
+		super(context, resource, objects);
+		vi = (LayoutInflater) context
+				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		Resource = resource;
+		factList = objects;
+	}
+ 
+	
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		// convert view = design
+		View v = convertView;
+		if (v == null) {
+			holder = new ViewHolder();
+			v = vi.inflate(Resource, null);
+			holder.imageview = (ImageView) v.findViewById(R.id.ivImage);
+			holder.tvName = (TextView) v.findViewById(R.id.tvName);
+			holder.tvDescription = (TextView) v.findViewById(R.id.tvDescriptionn);
+			
+			v.setTag(holder);
+		} else {
+			holder = (ViewHolder) v.getTag();
+		}
+		holder.imageview.setImageResource(R.drawable.ic_launcher);
+		new DownloadImageTask(holder.imageview).execute(factList.get(position).getImage());
+		holder.tvName.setText(factList.get(position).getName());
+		holder.tvDescription.setText(factList.get(position).getDescription());
+		return v;
+
+	}
+
+	static class ViewHolder {
+		public ImageView imageview;
+		public TextView tvName;
+		public TextView tvDescription;
+		
+
+	}
+
+	private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+		ImageView bmImage;
+
+		public DownloadImageTask(ImageView bmImage) {
+			this.bmImage = bmImage;
+		}
+
+		protected Bitmap doInBackground(String... urls) {
+			String urldisplay = urls[0];
+			Bitmap mIcon11 = null;
+			try {
+				InputStream in = new java.net.URL(urldisplay).openStream();
+				mIcon11 = BitmapFactory.decodeStream(in);
+			} catch (Exception e) {
+				Log.e("Error", e.getMessage());
+				e.printStackTrace();
+			}
+			return mIcon11;
+		}
+
+		protected void onPostExecute(Bitmap result) {
+			bmImage.setImageBitmap(result);
+		}
+
+	}
+}
